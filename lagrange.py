06a4832ad3d9cc8423626da4232708743e4b50d0
Michal Zajac <m.p.zajac@gmail.com>
2022-02-16 17:31:38 +0100

    removed additional code in lagrange.py

--- lagrange.py
+++ lagrange.py
@@ -1,30 +1,6 @@
 from pyfinite import ffield
 import functools   
 
-"""
-def evaluate_polynomial (field_size, lagrange_basis_evaluation_points, evaluation_point):
-    sum = 0
-    for i in range(len(lagrange_basis_evaluation_points)):
-        lagrange_basis_evaluation_points_reduced = list(lagrange_basis_evaluation_points)
-        del lagrange_basis_evaluation_points_reduced[i]
-        numer_list[:] = [evaluation_point - basis_evaluation_point for basis_evaluation_point in lagrange_basis_evaluation_points_reduced]
-        numer = functools.reduce(lambda a, b: a * b, numer_list)
-        denom_list[:] = [lagrange_basis_evaluation_points[i] - basis_evaluation_point for basis_evaluation_point in lagrange_basis_evaluation_points_reduced]
-        denom = functool.reduce(lambda a, b: a * b, denom_list)
-        sum = sum + numer / denom
-"""
-def lagrange_basis_evaluated (field, lagrange_basis_evaluation_points, evaluation_point):
-    lagrange_basis_evaluated_polynomial = []
-    for i in range(len(lagrange_basis_evaluation_points)):
-        lagrange_basis_evaluation_points_reduced = list(lagrange_basis_evaluation_points)
-        del lagrange_basis_evaluation_points_reduced[i]
-        numer_list[:] = [field.Subtract(evaluation_point,  basis_evaluation_point) for basis_evaluation_point in lagrange_basis_evaluation_points_reduced]
-        numer = functools.reduce(lambda a, b: field.Multiply(a, b), numer_list)
-        denom_list[:] = [field.Subtract(lagrange_basis_evaluation_points[i], basis_evaluation_point) for basis_evaluation_point in lagrange_basis_evaluation_points_reduced]
-        denom = functool.reduce(lambda a, b: field.Multiply(a, b), denom_list)
-        lagrange_basis_evaluated_polynomial[i] = field.Add(sum, field.Divide(numer, denom))
-return lagrange_basis_evaluated_polynomial
-
 def lagrange_interpolation_evaluation (field, list_base_evaluation_points,
                             list_base_evaluations, evaluation_point):
     evaluation = 0
@@ -37,7 +13,3 @@ def lagrange_interpolation_evaluation (field, list_base_evaluation_points,
     
         evaluation = field.Add(evaluation, field.Multiply(value_at_base_evaluation_point, list_base_evaluations[list_base_evaluation_points.index(base_evaluation_point)])) 
     return evaluation
-
-def evaluate_polynomial (field, lagrange_basis_evaluation_points, lagrange_basis_evaluations,  evaluation_point):
-    lagrange_basis_evaluated_polynomial = lagrange_basis_evaluated (field, lagrange_basis_evaluation_points, evaluation_point)
-    return sum(a * b for a, b in zip(lagrange_basis_evaluated_polynomial, lagrange_basis_evaluations))
