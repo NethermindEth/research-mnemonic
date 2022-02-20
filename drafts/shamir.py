3784d05918f488c9f163efdfea51ee4ed66b1adb
Ahmet Ramazan Agirtas <a.r.agirtas@gmail.com>
2022-02-21 00:29:42 +0300

    Update shamir.py
    Sampling random integer method is fixed.

--- drafts/shamir.py
+++ drafts/shamir.py
@@ -1,34 +1,31 @@
 from random import randint
-import numpy as np
 import galois
 
-
 #parameters
-q = 2**11
-N = 1
+q = 2**132
+N = 12
 GF11 = galois.GF(q)
-
-#This secret is for testing. Normally it will be taken from the words.txt file
-Secret = randint(1, q-1)
-# n: number of shareholders and t: threshold
-n = 15
-t = 7
 
 #The Dealer determines the n (number of shares) and t (threshold value)
 #The Dealer chooses a random polynomial f(x) of degree t-1 such that constant term is the secret to be shared.
 #The Dealer computes the share for the i-th user which is (i, f(i))
-def share_generation(n, t, secret):  
-    coefficient = np.random.randint(0, GF11.order, t, dtype=int)
-    coefficient[t-1] = secret
+def share_generation(num_share, threshold, secret):  
+    coeff =[]
+    for i in range(threshold): 
+        coeff.append(randint(0,q))
+    
+    coeff[threshold-1] = secret
+    coefficient = GF11(coeff)
+
     poly = galois.Poly(coefficient, field = GF11)
-    print('\n', poly, '\n')
-    eval_point = GF11(list(range(1, n+1)))
+    #print('\n', poly, '\n')
+    eval_point = GF11(list(range(1, num_share + 1)))
     return poly(eval_point)
 
 #Reconstruction (Lagrange Interpolation)
 def Lagrange_Interpolation(x=[], y=[]):
     if len(x) == len(y):
-        result = GF11.Zeros(N)
+        result = GF11.Zeros(1)
         for i in range(len(x)):
             LagrangeCoefficient = GF11([1])
             for j in range(len(x)):
@@ -37,10 +34,15 @@ def Lagrange_Interpolation(x=[], y=[]):
             result  = result + LagrangeCoefficient * y[i]  
     return result
 
-
 #------------------------------------------------------------------------------------------------------------------------------------
 #Driver Code
 #------------------------------------------------------------------------------------------------------------------------------------
+#This secret is for testing. Normally it will be taken from the words.txt file
+Secret = randint(1, q-1)
+# n: number of shareholders and t: threshold
+n = 15
+t = 7
+
 #sharing
 share_list = share_generation(n, t, Secret)
 
@@ -48,7 +50,7 @@ print('\nThe secret to be shared is:', Secret, '\n')
 print('The shares are: \n')
 
 for i in range (len(share_list)):
-    print('(', i+1, share_list[i], ')')
+    print('(', i+1, ',', share_list[i], ')')
 
 #here we define an access set which includes the x coordinates of t shares.
 access = list(range(1,t+1))
@@ -58,8 +60,12 @@ for i in access:
     access_share.append(share_list[i-1])
 print('\nThe access set: \n')
 for i in access:
-    print (i, access_share[i-1])
+    print ('(',i,',', access_share[i-1], ')')
     
 #reconstruction
 result_prime = Lagrange_Interpolation(access_set, access_share)
-print ('\nThe reconstructed secret is: \n', int(result_prime), '\n')
+print ('\nThe reconstructed secret is:', int(result_prime))
+if int(result_prime) == Secret:
+    print('Result:', True)
+else:
+    print('Result:', False)
