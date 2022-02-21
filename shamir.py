c8046cbcac4e38c1fd2ef7dfa73b3253be538dd3
jorem <34720266+jorem321@users.noreply.github.com>
2022-02-21 14:58:29 -0600

    reformatted code

---
+++ shamir.py
@@ -0,0 +1,48 @@
+import galois
+from random import randint
+
+
+def share_generation(secret, num_shares, threshold, num_words=12):
+    """Implements Shamir secret sharing.
+
+    This Shamir secret sharing implementation chooses a random polynomial f(x) of degree t-1
+    such that the constant term is the secret to be shared.
+    The secret is split into a number of shares, denoted num_shares. 
+    Each share is given by (i, f(i)) for 1<=i<=num_shares, and returned as a list.
+
+    Keyword arguments:
+    secret -- secret that will be encrypted.
+    num_shares -- number of shares that will be generated.
+    threshold -- minimum number of shares needed to reconstruct the secret.
+    num_words -- number of words that will be used in the mnemonic encoding the secret. 
+        (Default value chosen above follow a BIP-39 seed mnemonic implementation)
+    """
+
+    #Sanity checks between the given parameters.
+    assert(num_shares>=threshold, 'Threshold cannot be larger than the number of shares!')
+    assert(2**(11*num_words)>=secret, 'More words are needed to encode this secret!')
+
+    q=2**(11*num_words)
+    GF11 = galois.GF(q)
+    coeff =[]
+    for _ in range(threshold): 
+        coeff.append(randint(0,q))
+        
+    coeff[threshold-1] = secret
+    coefficient = GF11(coeff)
+
+    poly = galois.Poly(coefficient, field = GF11)
+    eval_point = GF11(list(range(1, num_shares + 1)))
+    return poly(eval_point)
+
+"""Implementation of Lagrange interpolation, used to reconstruct the secret from the shares generated above"""
+def lagrange_interpolation(x=[], y=[]):
+        if len(x) == len(y):
+            result = GF11.Zeros(1)
+            for i in range(len(x)):
+                LagrangeCoefficient = GF11([1])
+                for j in range(len(x)):
+                    if i != j:
+                        LagrangeCoefficient = LagrangeCoefficient * (x[j] / (x[j] - x[i]))
+                result  = result + LagrangeCoefficient * y[i]  
+        return result
---
+++ testing.py
@@ -0,0 +1,39 @@
+from random import randint
+from shamir import *
+import galois
+
+#------------------------------------------------------------------------------------------------------------------------------------
+#Driver Code
+#------------------------------------------------------------------------------------------------------------------------------------
+#Parameters for testing
+num_words = 12
+q=2**(11*num_words)
+num_shares = 15
+threshold = 7
+secret = randint(1, q-1)
+
+#Run Shamir's secret sharing and store the shares
+share_list = share_generation(secret, num_shares, threshold, num_words)
+print('\nThe secret to be shared is:', secret, '\n')
+print('The shares are: \n')
+for i in range (len(share_list)):
+    print('(', i+1, ',', share_list[i], ')')
+
+#here we define an access set which includes the x coordinates of t shares.
+GF11 = galois.GF(q)
+access = list(range(1,t+1))
+access_set = GF11(access)
+access_share = []
+for i in access:
+    access_share.append(share_list[i-1])
+print('\nThe access set: \n')
+for i in access:
+    print ('(',i,',', access_share[i-1], ')')
+    
+#reconstruction
+result_prime = lagrange_interpolation(access_set, access_share)
+print ('\nThe reconstructed secret is:', int(result_prime))
+if int(result_prime) == secret:
+    print('Result:', True)
+else:
+    print('Result:', False)
