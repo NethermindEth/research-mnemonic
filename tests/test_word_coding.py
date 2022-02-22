5591f199ca53b6c84a32ce68402e81d9434ce4da
jorem <34720266+jorem321@users.noreply.github.com>
2022-02-22 11:38:37 -0600

    turned test scripts into pytests

---
+++ tests/test_word_coding.py
@@ -0,0 +1,26 @@
+from word_coding import *
+
+###TODO: if necessary, change print statements into logging statements.
+
+
+def test_word_coding():
+    word_list = text_to_list('wordlist.txt')
+
+    seed_phrase = [
+        'wedding',
+        'web',
+        'trust',
+        'gadget',
+        'forum',
+        'calm',
+        'cannon',
+        'busy'
+        ]
+
+    #print(f'Original mnemonic: {seed_phrase}')
+    number = encode_words(word_list, seed_phrase)
+    #print (f'Obtained secret number {number}')
+    reconstructed_seed = decode_words(word_list, number)
+    #print (f'Re-created mnemonic {reconstructed_seed}')
+
+    assert reconstructed_seed==seed_phrase, 'Reconstructed secret does not match'
