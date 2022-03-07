20411e5ca5b08d77ef22f2552c7b502f27c772ba
jorem <34720266+jorem321@users.noreply.github.com>
2022-03-07 10:31:52 -0600

    test now takes random number of words for secret

--- tests/test_word_coding.py
+++ tests/test_word_coding.py
@@ -7,8 +7,8 @@ from word_coding import *
 def test_word_coding():
     word_list = text_to_list('wordlist.txt')
 
-    for _ in range(5):
-        seed_phrase_length = 12
+    for _ in range(10):
+        seed_phrase_length = randint(1,20)
         seed_phrase = sample(word_list, seed_phrase_length)
 
         number = encode_words(word_list, seed_phrase)
