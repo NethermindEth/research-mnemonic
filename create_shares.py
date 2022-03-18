519ef24c9198594dd138f7af71731ccb7db86ca0
jorem <34720266+jorem321@users.noreply.github.com>
2022-03-17 19:46:00 -0600

    allow non-verbose shares and custom secret path

--- create_shares.py
+++ create_shares.py
@@ -1,38 +1,63 @@
 from modules.get_primitive_poly import get_primitive_poly
 from modules import shamir, word_coding
 import argparse
+import glob
 import json
+import re
+import os
 
+#Empty shares folder to avoid errors
+files = glob.glob('shares/*')
+for f in files:
+    os.remove(f)
 
 #Parser is used in taking the number of shares(-n) and threshold (-t) from terminal.
 #Can also provide secret phrase directly into the terminal, as a space-separated phrase.
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', type=int, help='number of shares to generate', required=True)
-parser.add_argument('-t', type=int, help='threshold: number of shares to reconstruct the secret', required=True)
-parser.add_argument('-w', type=str, help='words to be shared as secret', required=False)
+parser.add_argument('--threshold', '-t', type=int, help='number of shares to reconstruct the secret', required=True)
+parser.add_argument('--secret', '-s', type=str, help='words to be shared as secret, as a space-separated string or as a path to a .txt file')
+parser.add_argument('--verbose', '-v', action='store_true', help='include public reconstruction data in the JSON shares?')
+
 args = parser.parse_args()
-assert args.n >= args.t, 'Number of shares(-n) cannot be lower than threshold(-t)!'
-n = args.n 
-t = args.t
-secret = args.w
+n = args.n
+t = args.threshold
+assert n >= t, 'Number of shares(-n) cannot be lower than threshold(-t)!'
+secret = args.secret
+verbose = args.verbose
 
 #Read dictionary, which should have 2^(nb) words for some integer nb.
 word_list = word_coding.text_to_list('wordlist.txt')
 nb = word_coding.get_dictionary_bits(word_list)
 
-#If no secret was provided in the terminal, read secret from text file 'secret.txt'. It is assumed 
-# that the text file will contain a single row of words, separated by a space.
-if secret == None:
-    with open('secret.txt', 'r') as file:
+# Obtain the secret. Begin considering the possibility that it will be loaded from a .txt file.
+if secret != None:
+    regex = r"^[A-Za-z]:(\\|\/).*\.[tT][xX][tT]"
+    print(secret)
+    file_path = re.match(regex, secret.strip())[0]
+    print("file path")
+    print(file_path)
+else:
+    #If no secret was provided, we will be loading secret.txt by default.
+    file_path = 'secret.txt'
+
+# If a file path was found, load it.
+if file_path != None:
+    # It is assumed that the text file will contain a single row of words, separated by a space.
+    with open(file_path, 'r') as file:
         secret = file.read()
 
-#All words in the secret should be in the dictionary.
+#If no file path was found above, then -s is already the secret phrase! 
 secret = secret.strip().split(' ')
+print(secret)
+
+#All words in the secret should be in the dictionary.
 assert set(secret).issubset(set(word_list)), "Not every word in the secret is contained in the dictionary"
 
 #Constructing Galois fields, using the number of words of the secret.
 #Get the primitive polynomial from the provided JSON.
 nw = len(secret)
+assert nw <= 60, "Secret can only have up to 60 words."
 q = 2**(nb*nw)
 primitive_poly = get_primitive_poly(nb*nw)
 print('primitive_poly: ', primitive_poly)
@@ -42,24 +67,29 @@ binary_secret = word_coding.encode_words(word_list, secret)
 decimal_secret = int(binary_secret,2)
 shares = shamir.share_generation(decimal_secret, n, t, q, primitive_poly)
 
-#For each share, program outputs a file including id, shared secret(word encoded), degree and irr poly to construct unique GF.
+#Output public reconstruction data, which will be added to each share if --verbose is true:
+reconstruction_data = {
+    'total_shares' : n,
+    'threshold' : t,
+    'primitive_poly' : primitive_poly,
+    'dictionary' : ' '.join(word_list)
+    }
+with open('shares/reconstruction_data.json', 'w') as file:
+    json.dump(reconstruction_data, file, indent=4)
 
+#For each share, output a JSON file including id and secret share (word-encoded).
 for i in range (len(shares)):
     reconstructed_shared_secrets = word_coding.decode_words(word_list, format(shares[i], "b").zfill(nb*nw))
     print('id: ', i+1)
     print('share: ',reconstructed_shared_secrets)
 
     file_name = "shares/share_" + str(i+1) + ".json"
-    with open(file_name, 'w') as file:
-        json.dump(
-            {
+    share_data = {
             'id': i+1,
-            'total_shares' : n,
-            'threshold' : t,
             'share' : reconstructed_shared_secrets,
-            'primitive_poly' : primitive_poly,
-            'dictionary' : ' '.join(word_list)
-            },
-            file, 
-            indent=4
-            )
+            }
+    if verbose:
+        share_data.update(reconstruction_data)
+
+    with open(file_name, 'w') as file:
+        json.dump(share_data, file, indent=4)
--- reconstruct.py
+++ reconstruct.py
@@ -1,31 +1,63 @@
+from msilib.schema import Error
 from modules import shamir, word_coding
+import argparse
 import glob
 import json
 
+def get_polynomial_degree(polynomial):
+	monomials = polynomial.split('+')[0].strip()
+	return int(monomials[2:])
+
+def get_reconstruction_data(dict):
+	return (
+		dict['total_shares'],
+		dict['threshold'],
+		dict['primitive_poly'],
+		dict['dictionary'].split(' ')
+		)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--verbose', '-v', action='store_true', help='do JSON shares include public reconstruction data?')
+args = parser.parse_args()
+verbose = args.verbose
 
 #Initialize lists:
 x_id = []
 y_shares = []
 
 #Search JSON files with names of the form 'share_i.json', where 'i' is an integer.
-#Extract ID and mnenmonic share data.
-first_iteration = True
-for json_filename in glob.glob('shares/share*[0-9].json'): #TODO: does this work for numbers greater than 10?
+list_of_share_filenames = glob.glob('shares/share*[0-9].json')
+
+#Extract reconstruction data. It must be provided externally if the shares are not verbose.
+#Otherwise, one of the shares is picked.
+reconstruction_json_path = list_of_share_filenames[0] if verbose else 'shares/reconstruction_data.json'
+with open(reconstruction_json_path, "r") as json_file:
+	data = json.load(json_file)
+try:
+	total_shares, threshold, primitive_poly, word_list = get_reconstruction_data(data)
+except KeyError:
+	if verbose:
+		raise RuntimeError('-v flag was added, but not all the shares are verbose')
+	else:
+		raise RuntimeError('shares/reconstruction_data.json does not contain all the reconstruction data')
+		
+dict_bits = word_coding.get_dictionary_bits(word_list)
+num_words = get_polynomial_degree(primitive_poly)//dict_bits
+q = 2**(dict_bits*num_words)
+
+#Extract ID and mnenmonic share data from each share
+for json_filename in list_of_share_filenames: #TODO: does this work for numbers greater than 10?
 	with open(json_filename, "r") as json_file:
 		json_share = json.load(json_file)
 
-	#Dictionary and primitive polynomial should be consistent amongst shares.
-	if(first_iteration):
-		threshold = json_share['threshold']
-		word_list = json_share['dictionary'].split(' ')
-		dict_bits = word_coding.get_dictionary_bits(word_list)
-		num_words = len(json_share['share'])
-		q = 2**(dict_bits*num_words)
-		primitive_poly = json_share['primitive_poly']
-		expected_data = [json_share['total_shares'], threshold, primitive_poly, json_share['dictionary']]
-	else:
-		assert expected_data == [json_share['total_shares'], json_share['threshold'], primitive_poly, json_share['dictionary']], 'Shares have incompatible data'
-	first_iteration = False
+	#Check the consistency of the reconstruction data (if verbose) and number of words.
+	if verbose:
+		try:
+			assert get_reconstruction_data(json_share) == (total_shares, threshold, primitive_poly, word_list), 'Shares have incompatible reconstruction data.'
+		except KeyError:
+			raise ShareNotVerboseError('-v flag was added, but not all the shares are verbose')
+			
+	assert len(json_share['share']) == num_words, 'A share has a number of words inconsistent with the given polynomial and dictionary'
 
 	x_id.append(json_share['id'])
 	binary_share = word_coding.encode_words(word_list, json_share['share'])
@@ -38,3 +70,4 @@ seed_phrase = word_coding.decode_words(word_list, format(reconstructed_secret, "
 print(seed_phrase)
 with open('secret_reconstructed.txt', 'w') as file:
 	file.write(" ".join(seed_phrase))
+
