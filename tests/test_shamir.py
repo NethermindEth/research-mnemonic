e1bbdae920863e58228c587af4f73c41b329fed2
jorem <34720266+jorem321@users.noreply.github.com>
2022-03-22 14:59:58 -0600

    fix test

--- tests/test_shamir.py
+++ tests/test_shamir.py
@@ -11,15 +11,15 @@ def test_shamir():
         #Parameters for testing
         num_words = randint(3,30)
         q=2**(11*num_words)
-        irreducible_poly = get_primitive_poly(11*num_words)
+        primitive_poly = get_primitive_poly(11*num_words)
         shared_secret = randint(1,q-1)
         num_shares = randint(5,15)
         threshold = randint(3, num_shares)
 
         #Run Shamir's secret sharing and store the shares
-        shares = share_generation(shared_secret, num_shares, threshold, q, irreducible_poly)
+        shares = share_generation(shared_secret, num_shares, threshold, primitive_poly)
         pick_indices = sample(list(range(1,num_shares+1)), threshold)
         shares_picked = [shares[index-1] for index in pick_indices]
-        reconstructed_secret = secret_reconstruction(pick_indices, shares_picked, q, irreducible_poly)
+        reconstructed_secret = secret_reconstruction(pick_indices, shares_picked, primitive_poly)
 
         assert reconstructed_secret == shared_secret, 'A reconstructed secret does not match the original'
